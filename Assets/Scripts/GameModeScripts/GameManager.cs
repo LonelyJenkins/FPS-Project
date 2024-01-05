d394fffa1891f86b66784d436d948d420cf360d7
LonelyJenkins <124760225+LonelyJenkins@users.noreply.github.com>
2024-01-05 11:59:04 -0600

    Reorganization of Assets
    Removal of unused assets and organizing asset folder for ease of use

--- Assets/Scripts/GameModeScripts/GameManager.cs
+++ Assets/Scripts/GameModeScripts/GameManager.cs
@@ -42,18 +42,23 @@ public class GameManager : MonoBehaviour
     {
         playerController = player.GetComponent<PlayerController>();
         friendlyCount += startingFriendlyCount;
+        //Begins rounds with predetermined amount of friendly NPCs
         waveNumber++;
+        //Begins wave count
         lastBossWave = 0;
+        //Data used to calculate Boss wave intervals
         NextWave();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //This will keep track of all data for HUD
         enemyCounter.text = ("Enemies Left: " + enemyCount);
         doorCounter.text = ("Cabins Remaining: " + doorsRemaining);
         friendlyCounter.text = ("Survivors Alive: " + friendlyCount);
 
+        //Check for player death.
         if (playerController.isDead == true)
         {
             waveDisplay.enabled = true;
@@ -61,11 +66,13 @@ public class GameManager : MonoBehaviour
             return;
         }
 
+        //Checks if all cabin doors have been destroyed. This will determine if any ammo/friendlies continue to spawn in
         if (doorsRemaining <= 0)
         {
             allDoorsDestroyed = true;
         }
 
+        //Ends round once all enemies have been eliminated
         if (isEnemyWave && enemyCount <= 0 && !roundOver)
         {
             roundOver = true;
@@ -77,6 +84,8 @@ public class GameManager : MonoBehaviour
             waveDisplay.text = ("WAVE " + waveNumber + " IS COMING");
             StartCoroutine(RoundInterval());
         }
+
+        //Ends round for Boss Wave if all bosses are elminated
         if (isBossWave && bossCount <= 0 && !roundOver)
         {
             roundOver = true;
@@ -92,12 +101,14 @@ public class GameManager : MonoBehaviour
 
     void NextWave()
     {
+        //Calculates the amount of enemies to spawn in for current wave
         roundOver = false;
         enemyCount = lastEnemyCount;
         enemyCount += 4;
         lastEnemyCount = enemyCount;
         isEnemyWave = true;
 
+        //Spawns randomly chosen zombie prefabs at randomly chosen spawn points and triggers their isAlerted bool on spawn
         for (int i = 0; i < enemyCount; i++)
         {
             int enemyIndex = Random.Range(0, enemies.Length);
@@ -110,6 +121,7 @@ public class GameManager : MonoBehaviour
 
     void BossWave()
     {
+        //Takes data from previous bossWave to determine amount of BossMonsters to spawn in. Also registers the current bossWave for future usage
         roundOver = false;
         lastBossWave = waveNumber;
         bossCount = lastBossCount;
@@ -117,6 +129,7 @@ public class GameManager : MonoBehaviour
         lastBossCount = bossCount;
         isBossWave = true;
 
+        //Spawns Boss monster at randomly chosen spawn points and triggers their isAlerted bool
         for (int i = 0; i < bossCount; i++)
         {
             int spawnIndex = Random.Range(0, enemySpawnPoints.Length);
@@ -127,17 +140,18 @@ public class GameManager : MonoBehaviour
 
     public void SpawnFriendly()
     {
+        //If not all cabin doors are destroyed, begin spawning friendlies
         if (!allDoorsDestroyed)
         {
             foreach (GameObject friendlySpawn in friendlySpawnPoints)
             {
                 if(friendlySpawn.activeInHierarchy != false)
-                {
+                {//this checks if cabin door has been destroyed before attempting to spawn in friendly
                     int dropChance = Random.Range(0, 2);
                     if (dropChance == 1)
                     {
-                        friendlyCount++;
-                        int friendlyIndex = Random.Range(0, friendlies.Length);
+                        friendlyCount++; //updates the friendly NPC count in the HUD
+                        int friendlyIndex = Random.Range(0, friendlies.Length); //chooses random friendly prefab
                         Instantiate(friendlies[friendlyIndex], friendlySpawn.transform.position, Quaternion.identity);
                     }
 
@@ -148,6 +162,7 @@ public class GameManager : MonoBehaviour
 
     void SpawnAmmo()
     {
+        //Spawns ammunition for player in similar fashion to friendlies
         if (!allDoorsDestroyed)
         {
             Debug.Log("AMMO HAS SPAWNED! COLLECT WHILE YOU CAN");
@@ -165,6 +180,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator RoundInterval()
     {
+        //Gives player short period to prepare for incoming wave. This also determines if incoming wave should be Normal or Bosswave
         yield return new WaitForSeconds(roundIntervalTime);
 
         waveDisplay.enabled = false;
