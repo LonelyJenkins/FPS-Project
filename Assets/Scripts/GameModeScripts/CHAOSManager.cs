1af23ea9628095c5f173e8be1170b0d506f18479
LonelyJenkins <124760225+LonelyJenkins@users.noreply.github.com>
2024-01-29 13:58:33 -0600

    CHAOS Map created
    Map created as a duplicate of TDM map. No logic yet implemented.

---
+++ Assets/Scripts/GameModeScripts/CHAOSManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CHAOSManager : MonoBehaviour
+{
+    //logic for chaos mode will go here.
+    //Very similar to TDM but with zombie spawner continually spawning in zombies and occasionally boss monsters. Zombies will count towards player kills, however overall team score will remain unaffected.
+    //NOT FINAL: Winning game mode may be dependent on how many deaths occur on opposing teams, rather than killcounts. This will be determined soon
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
